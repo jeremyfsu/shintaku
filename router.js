266eca02ab7d457571dceb467a70601fa3d3c2c6
Jeremy Walworth <jeremy@jeremywalworth.com>
2014-12-01 04:17:02 +0000

    worked on the "getter"

--- router.js
+++ router.js
@@ -4,6 +4,12 @@ $(function() {
   Vault.list();
  });
  
+ Finch.route("get", function() {
+  Finch.observe(function(params) {  
+   return console.log(params('key'));
+  });
+ });
+
  Finch.route("/", function() {
   Finch.navigate("list");
  });
